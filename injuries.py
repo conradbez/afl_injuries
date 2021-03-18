a91ff9b5a329e5700f991361f8ad1222b96f0eea
Your Name <test>
2021-03-18 20:21:06 +0800

    init

---
+++ injuries.py
@@ -0,0 +1,27 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+import requests
+import pandas as pd
+
+
+# %%
+r = requests.get('https://www.afl.com.au/news/564/afl-injury-list')
+dfs = pd.read_html(r.text)
+
+
+# %%
+df = pd.DataFrame()
+for df_part in dfs:
+    updated_text = df_part.iloc[-1,0]
+    df_part = df_part.iloc[:-1]
+    date = updated_text[updated_text.find(':')+2:]
+    df_part['Updated'] = date
+    df = df.append(df_part)
+df.to_csv('injuries.csv')
+
+
+# %%
+
+
+
